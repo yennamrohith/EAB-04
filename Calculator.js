71dafa869f35bf90a808a1438686379921bd3775
Yennam Rohith <yennamrohith1772304@gmail.com>
2024-08-21 12:56:03 +0530

    Add Initial Files

---
+++ Calculator.js
@@ -0,0 +1,21 @@
+function appendNumber(number) {
+    document.getElementById('display').value += number;
+}
+function appendOperator(operator) {
+    document.getElementById('display').value += operator;
+}
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+function deleteLast() {
+    let display = document.getElementById('display');
+    display.value = display.value.slice(0, -1);
+}
+function calculateResult() {
+    let display = document.getElementById('display');
+    try {
+        display.value = eval(display.value);
+    } catch (e) {
+        display.value = 'Error';
+    }
+}
